6dcd49768f259e1db4e00e09d83fcabbea470f76
Bartek <bartraw23@gmail.com>
2020-07-08 20:09:14 +0200

    Add project base

---
+++ animal-data/src/main/java/com/springshelllter/animaldata/AnimalDataApplication.java
@@ -0,0 +1,13 @@
+package com.springshelllter.animaldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalDataApplication.class, args);
+	}
+
+}
---
+++ animal-data/src/test/java/com/springshelllter/animaldata/AnimalDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springshelllter.animaldata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
