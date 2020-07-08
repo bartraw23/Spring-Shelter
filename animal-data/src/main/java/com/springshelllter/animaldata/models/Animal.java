7bf02bf841c7629775aed2d35e75009d757941c8
Bartek <bartraw23@gmail.com>
2020-07-08 20:58:22 +0200

    Apply getters and setters to animal

--- animal-data/src/main/java/com/springshelllter/animaldata/models/Animal.java
+++ animal-data/src/main/java/com/springshelllter/animaldata/models/Animal.java
@@ -36,4 +36,52 @@ public class Animal {
 
     @Column
     private LocalDateTime dateOfEnrollment;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDateTime getDateOfEnrollment() {
+        return dateOfEnrollment;
+    }
+
+    public void setDateOfEnrollment(LocalDateTime dateOfEnrollment) {
+        this.dateOfEnrollment = dateOfEnrollment;
+    }
 }
