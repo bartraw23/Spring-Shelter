9a5079841d5e9363b01a6409d5fbe57c0bb8b67b
Bartek <bartraw23@gmail.com>
2020-07-08 20:09:35 +0200

    Start creation of animal class

---
+++ animal-data/src/main/java/com/springshelllter/animaldata/models/Animal.java
@@ -0,0 +1,32 @@
+package com.springshelllter.animaldata.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.UUID;
+
+@Entity
+public class Animal {
+    @Id
+    @GeneratedValue
+    @JsonIgnore
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    private UUID id;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String species;
+
+    @Column
+    private String race;
+}
