d6ed0c6ec0a1a30d8e7b60c89484ba063fe533e5
Bartek <bartraw23@gmail.com>
2020-07-10 00:20:21 +0200

    Init for view MS

---
+++ view-microservice/src/main/java/com/springshellter/viewmicroservice/ViewMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springshellter.viewmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewMicroserviceApplication.class, args);
+	}
+
+}
---
+++ view-microservice/src/test/java/com/springshellter/viewmicroservice/ViewMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springshellter.viewmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
