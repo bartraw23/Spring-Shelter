8b3e0270ed48af95e3d7b56d9ced8a5ed7780299
Bartek <bartraw23@gmail.com>
2020-07-15 22:32:48 +0200

    Fix  eureka for view services

--- view-microservice/src/main/java/com/springshelter/viewmicroservice/controller/ViewController.java
+++ view-microservice/src/main/java/com/springshelter/viewmicroservice/controller/ViewController.java
@@ -6,23 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/shelter")
 public class ViewController {
 
     @Autowired
     private AnimalService animalService;
+
+    @Autowired
+    WebClient.Builder webClientBuilder;
 
     @GetMapping
     public String viewIndex() {
         return "index";
     }
 
-    @GetMapping("form")
+    @GetMapping("/form")
     public String addAnimalForm() {
         return "form";
     }
