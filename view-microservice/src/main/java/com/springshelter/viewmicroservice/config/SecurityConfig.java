c4e93857890b21a2e1fd2779a6fdf3d91ef2081f
Bartek <bartraw23@gmail.com>
2020-08-07 22:01:38 +0200

    Script fix and token transfer start

--- view-microservice/src/main/java/com/springshelter/viewmicroservice/config/SecurityConfig.java
+++ view-microservice/src/main/java/com/springshelter/viewmicroservice/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.springshelter.viewmicroservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -13,6 +21,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/form", "/animals").authenticated()
                 .and()
-                .oauth2Login();
+                .oauth2Login()
+                .authorizedClientService(new OAuth2AuthorizedClientService() {
+                    @Override
+                    public <T extends OAuth2AuthorizedClient> T loadAuthorizedClient(String s, String s1) {
+                        return null;
+                    }
+
+                    @Override
+                    public void saveAuthorizedClient(OAuth2AuthorizedClient oAuth2AuthorizedClient, Authentication authentication) {
+
+                    }
+
+                    @Override
+                    public void removeAuthorizedClient(String s, String s1) {
+
+                    }
+                });
     }
 }
